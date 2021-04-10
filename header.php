f1b42a6658ba9edeb38701def8d64a4332200d07
Alekscostam <kowalski121.al@gmail.com>
2021-04-10 09:48:05 +0200

    navbar style changed

--- header.php
+++ header.php
@@ -13,17 +13,20 @@
 
    </script>
     <nav class="navbar navbar-light  navbar-expand-lg scrolling-navbar navbar-fixed-top ">
-        <a class="text-logo navbar-brand" href="https://akademiawojownika.herokuapp.com/"><span style="font-size:90%; font-weight:600 ;color: #cab80e;">AKADEMIA WOJOWNIKÓW</span></a>
-        <button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
-            <span class="fas fa-bars mx-auto" style="color:#cab80e; font-size:1.5rem;"></span>
+       
+   
+        <a class="text-logo " href="https://akademiawojownika.herokuapp.com/"><span style="font-size:90%; font-weight:600 ;color: #cab80e;">AKADEMIA WOJOWNIKÓW</span></a>
+        <button class="navbar-toggler mx-auto" type="button" style="margin:20px" data-toggle="collapse"  data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+            <span  class="fas fa-bars mx-auto" style="color:#cab80e; font-size:1.4rem; "></span>
         </button>
+   
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto " style="color: #cab80e;">
                 <li class="nav-item active">
                     <a class="nav-link" href="https://akademiawojownika.herokuapp.com/#Akademia"><span data-toggle="collapse"  data-target="#navbarNav"  class="bookmarks">AKADEMIA</span></a>
                 </li>
                 <li class="nav-item" >
-                    <a class="nav-link" href="TreningiOnline.php"><span data-toggle="collapse" data-target="#navbarNav" class="bookmarks">TRENINGI ONLINE</span></a>
+                    <a class="nav-link" href="TreningiOnline.php"><span data-toggle="collapse" data-target="#navbarNav"  class="bookmarks">TRENINGI ONLINE</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="https://akademiawojownika.herokuapp.com/#Zajęcia"><span data-toggle="collapse" data-target="#navbarNav" class="bookmarks">ZAJĘCIA</span></a>
