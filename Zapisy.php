ea3893c40bbf8d9765e5e6e79982691088703f07
Alekscostam <kowalski121.al@gmail.com>
2021-05-25 15:10:45 +0200

    something

---
+++ Zapisy.php
@@ -0,0 +1,132 @@
+
+<style>
+    <?php include './CSS/style.css'; ?><?php include './js/main.js'; ?>
+</style>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Akademia Wojowników</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+    <link href="CSS/style.css" rel="stylesheet">
+
+    <script type="text/javascript" src="./js/main.js"></script>
+    <link rel="stylesheet" href="https://m.w3newbie.com/you-tube.css">
+    <link rel="stylesheet" type="text/css" href="./lightbox.css">
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+    <link href='https://css.gg/add.css' rel='stylesheet'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link src="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+        <?php include('./header.php'); ?>
+    </header>
+    <!--- Image Slider -->
+
+
+
+    <section id="treningi" style="padding-top: 88px;;">
+        <div class="container-fluid padding justify-content-center">
+            <div class="col">
+                <h2 class="header-info" style="font-weight: 900; font-size: 170%;">Zapisz swoje dziecko do akademii wojowników</h2>
+
+            </div>
+            <br>
+            <div class="row text-center padding w-75 " style="margin: auto;">
+
+                <div class="col-lg-12">
+                    <a href="#" style="font-size:100%;  padding: 0.8rem 2rem;  color: #f4f4f4;  text-transform: uppercase; border: 1px; border-style: solid; border-color: #ffe600;" class="btn">Zapisy</a>
+
+                    <br><br>
+                    <div>
+                        <h5 class="header-content">
+                            Tutaj będzie isntrukcja dla uzytkownika, dotycząca systemu służącego do zapisów
+                        </h5>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <h2 class="header-info" style="font-weight: 400; font-size: 110%;"><i class="fas fa-map-marker"></i>&nbsp;<b>Bydgoskie Centrum Finansowe "Drukarnia" poziom -1 ul. Jagielonska 1</b></h2>
+            </div>
+        </div>
+    </section>
+    <br>
+    <section id="Treningi-personalne" ></section>
+    <div id="club-back">
+        <div class="container-fluid " >
+
+
+            <div class="row justify-content-center ">
+
+               
+                <div class="col-lg-4 col-md-4  " style=" margin:auto">
+                    <!-- <h2 id="data" style=" font-size: 140%;">01.04.2020</h2> -->
+                    <h2 class="header-info text-dark " style="font-weight: 900; font-size: 170%;">Dołącz do nas!</h2><hr  style="background:#ffe600;">
+                    <p></p>
+                    <h5 style="font-size: 111%;" class="text-center text-dark">
+                    
+                        <label class="desc " style="text-align: center!important;">
+                        Dołącz do Akademii Wojowników i bądź częścią naszego Teamu! Oferta kierowana jest do nauczycieli wf-u, pedagogów i osób pracujących z dziećmi.
+                    Przeszkolimy Cię w zakresie naszej metodyki prowadzenia zajęć.
+                    Razem zadbamy o zdrowie, kondycję i dobre samopoczucie naszych podopiecznych.
+                        </label>
+                    </h5>
+                    <hr style="background:#ffe600;">
+                </div> 
+                <div     class="col-lg-5 col-md-4  " style=" margin:auto">
+                    <!-- <h2 id="data" style=" font-size: 140%;">01.04.2020</h2> -->
+                    <h2 class="header-info text-dark" style="font-weight: 900; font-size: 170%;" >Treningi personalne</h2><hr style="background:#ffe600;">
+                    <p></p>
+                    <h5 style="font-size: 111%;" class="text-center text-dark">
+                        <label class="desc" style="text-align: center!important;">
+                        Akademia Wojowników oprócz zajęć grupowych oferuje także treningi personalne dla dzieci. 
+Jeżeli Tobie i Twojemu dziecku zależy na indywidualnym podejściu, wzmożonej uwadze trenera i wyższej skuteczności treningów, to zapraszamy do zapisów. Zachęcamy także rodziców do czynnego uczestnictwa w zajęciach, co z pewnością zaowocuje większym zaangażowaniem dziecka.
+                        </label>
+                    </h5>
+                    <hr style="background:#ffe600;">
+                </div>
+                <div class="col-lg-3 col-md-4  " style=" margin:auto">
+                    <!-- <h2 id="data" style=" font-size: 140%;">01.04.2020</h2> -->
+                    <h2 class="header-info text-dark" style="font-weight: 900; font-size: 170%;" >Przedszkola</h2><hr style="background:#ffe600;">
+                    <p></p>
+                    <h5 style="font-size: 111%;" class="text-center text-dark">
+                        <label class="desc" style="text-align: center!important;">
+                        Zapraszamy przedszkola do współpracy w zakresie prowadzenia zajęć sportowych.
+                    Treningi ogólnorozwojowe z elementami boksu i Kickbixingu oraz zajęć sportowych, sprawny przedszkolak.
+                        </label>
+                    </h5>
+                    <hr style="background:#ffe600;">
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
+    
+
+
+
+
+
+    <?php include('./footer.php'); ?>
+
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,71 @@
+const readMoreBtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.text-first');
+const accordingItemHeaders = document.querySelectorAll(".according-item-header");
+accordingItemHeaders.forEach(accordingItemHeaders => {
+    accordingItemHeaders.addEventListener("click", event => {
+
+        accordingItemHeaders.classList.toggle("active");
+    });
+});
+
+
+
+function checkform() {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var name = document.getElementById('name').value;
+
+    var surname = document.getElementById('surname').value;
+
+    var email = document.getElementById('email').value;
+
+    var message = document.getElementById('message').value;
+
+    var subject = document.getElementById('subject').value;
+
+
+    if (/^[A-z ]+$/.test(name) && /^[A-z ]+$/.test(surname) && re.test(String(email).toLowerCase()) && /^[A-z ]+$/.test(message) && /^[A-z ]+$/.test(subject)) {
+
+        return true;
+    } else {
+        document.getElementById('errorMessage').textContent = "Uzupełnij prawidłowo wszytskie informacje!";
+        return false;
+    }
+
+}
+
+
+function ShowOrHideTxt() {
+ 
+    var x = document.getElementById("right-div-hidden");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "block";
+  }
+ 
+  var z = document.getElementById("bottom-div-hidden");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "block";
+  }
+ 
+
+
+  var y = document.getElementById("read-more-btn");
+  y.style.display = "none";
+
+
+  document.getElementsByClassName("div-changer");
+
+
+}
+
+//  var one =   document.getElementById('moreText');
+//     if(document.getElementById('moreText').style.display=="none")
+//     {
+//         document.getElementById('moreText').style.display="inline";
+//     }
+
+
+
