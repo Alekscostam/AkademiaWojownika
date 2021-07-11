9485af133a6a195c85b0651243233739c0efbd99
Alekscostam <kowalski121.al@gmail.com>
2021-07-11 15:30:43 +0200

    some changes

--- index.php
+++ index.php
@@ -93,15 +93,17 @@ if (isset($_POST['submit'])) {
         </video>
     </div>
     
-    <section id="Akademia">
-    <h1 title="Sport Zabawa Integracja" class="text-dark pb-4 text-center top-text " id="header-styler" >
-    <div style="padding-top: 30px;" >
+    <section id="Akademia"></section>
+   
+    <div style="padding-top: 35px;" class="text-dark pb-4 text-center top-text " id="header-styler" >
+    <h1>
         <a class="sport " style="color:red; text-shadow: 0px 1px 1px #000!important;">SPORT</a>&nbsp;
         <a class="zabawa" style="color:black;  text-shadow: 0px 1px 1px #000!important;">ZABAWA</a>&nbsp;
         <a class="integracja" style="color:rgb(255, 200, 0);  text-shadow: 0px 1px 1px #000!important;">INTEGRACJA</a>&nbsp;
-        </div>
     </h1>
-    </section>
+    </div>
+    
+    
     <br>
     <div id="club-back">
         <div class="container">
